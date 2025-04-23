17fee145266c45c5847aa6db5604c7c634810a0a
Iffatuz Zahra <iffatuzzahra28@gmail.com>
2025-04-23 21:20:15 +0700

    menambahkan parameter untuk menyimpan data dari halaman sebelumnya dan selalu dibutuhkan di halaman ini

---
+++ lib/detail_order_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:navigation_routing_parsingparameter/home_page.dart';
+
+class DetailOrderPage extends StatelessWidget {
+  final String makanan;
+  final String minuman;
+  final String jumlahMakanan;
+  final String jumlahMinuman;
+  final int totalHarga;
+
+  const DetailOrderPage({
+    super.key,
+    
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
