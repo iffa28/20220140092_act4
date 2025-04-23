63a2f98deb2c59218b03230b58bd8381407ca58d
Iffatuz Zahra <iffatuzzahra28@gmail.com>
2025-04-23 20:49:16 +0700

    menambahkan controller untuk mengambil input dari textformfield

--- lib/order_page.dart
+++ lib/order_page.dart
@@ -9,16 +9,30 @@ class OrderPage extends StatefulWidget {
 }
 
 class _OrderPageState extends State<OrderPage> {
+  final TextEditingController makananController = TextEditingController();
+  final TextEditingController minumanController = TextEditingController();
+  final TextEditingController jumlahMakananController = TextEditingController();
+  final TextEditingController jumlahMinumanController = TextEditingController();
+  int totalHarga = 0;
+
   
 
   @override
   Widget build(BuildContext context) {
-
+    final _formKey = GlobalKey<FormState>();
     return Scaffold(
       appBar: AppBar(
         title: const Text('Order Page'),
       ),
-      
+      body: Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            
+            
+          ]
+        ),
+      ),
     );
   }
 }
