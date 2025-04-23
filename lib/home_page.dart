86a5f85fd05c55cb72f8264c94bf6224d638c11d
Iffatuz Zahra <iffatuzzahra28@gmail.com>
2025-04-23 19:42:42 +0700

    menambahkan halaman home dan navigator ke halaman order dengan push(seperti stack,jika berpindah halaman bisa kembali ke halaman sebelumnya)

---
+++ lib/home_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:navigation_routing_parsingparameter/order_page.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Selamat datang di Home Page!'),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const OrderPage(),
+                  ),
+                );
+              },
+              child: Text('Order Now'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
