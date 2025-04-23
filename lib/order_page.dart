811eee3abe16422d70f9794fd0847fa0b1226092
Iffatuz Zahra <iffatuzzahra28@gmail.com>
2025-04-23 20:46:46 +0700

    menambahkan statefulwidget untuk halaman order

---
+++ lib/order_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:navigation_routing_parsingparameter/detail_order_page.dart';
+
+class OrderPage extends StatefulWidget {
+  const OrderPage({super.key});
+
+  @override
+  State<OrderPage> createState() => _OrderPageState();
+}
+
+class _OrderPageState extends State<OrderPage> {
+  
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Order Page'),
+      ),
+      
+    );
+  }
+}
