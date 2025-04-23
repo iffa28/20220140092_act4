735da805982e8e5ae6b29b66f108833017ee6d80
Iffatuz Zahra <iffatuzzahra28@gmail.com>
2025-04-23 21:04:56 +0700

    menambahkan button untuk navigasi halaman dan mengirim data order

--- lib/order_page.dart
+++ lib/order_page.dart
@@ -75,6 +75,27 @@ class _OrderPageState extends State<OrderPage> {
                 return null;
               },
             ),
+            ElevatedButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  calculateTotalPrice();
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: 
+                      (context) => DetailOrderPage(
+                        jumlahMakanan: jumlahMakananController.text,
+                        jumlahMinuman: jumlahMinumanController.text,
+                        makanan: makananController.text,
+                        minuman: minumanController.text,
+                        totalHarga: totalHarga,
+                      ),
+                    ),
+                  );
+                }
+              },
+              child: const Text('Order Now'),
+            ),
             
           ]
         ),
